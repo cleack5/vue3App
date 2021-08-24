b9b31d6c2f562793aee3ecb172b6deab7b543373
yang fei <yangfei0610@gmail.com>
2021-08-25 00:46:51 +0800

    添加dev serve代理转发请求的配置

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,31 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server:{
+    proxy:{
+      // 字符串简写写法
+      '/foo': 'http://localhost:4567',
+      // 选项写法
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      // 正则表达式写法
+      '^/fallback/.*': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fallback/, '')
+      },
+      // 使用 proxy 实例
+      '/api2': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          // proxy 是 'http-proxy' 的实例
+        },
+      }
+    }
+  }
 })
