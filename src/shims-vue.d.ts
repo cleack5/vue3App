aa61582e73955b025c307e9c675b46af4195e1f8
yang fei <yangfei0610@gmail.com>
2021-08-26 01:01:30 +0800

    引入typescript，并添加对应配置进行开发

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,8 @@
+/**
+ * The shims-vue.d.ts file helps your IDE to understand what a file ending in .vue is.
+ */
+declare module '*.vue' {
+  import { defineComponent } from 'vue'
+  const Component: ReturnType<typeof defineComponent>
+  export default Component
+}
