2ca26346caecec8febb95362d0b3847422010d67
yang fei <yangfei0610@gmail.com>
2021-11-13 17:18:22 +0800

    网站代码添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Home</div>
+  <div class="page-root">
+    <div class="page-header"></div>
+    <div class="page-body"></div>
+    <div class="page-footer">
+      <div class="ipc-box">
+        <a href="https://beian.miit.gov.cn/">粤ICP备2021127505号</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,7 +17,38 @@
 </script>
 
 <style scoped lang="less">
-  a {
-    color: #42b983;
+  .page-root {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    .page-header,
+    .page-footer {
+      flex-grow: 0;
+      flex-shrink: 0;
+      height: 100px;
+    }
+
+    .page-body {
+      flex-grow: 1;
+    }
+
+    .page-footer {
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      align-items: center;
+
+      background-color: #e7e7e7;
+
+      .ipc-box {
+        a {
+          color: #222222;
+          text-decoration: none;
+          font-size: 14px;
+        }
+      }
+    }
   }
 </style>
